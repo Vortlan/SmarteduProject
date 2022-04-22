bacccf8111e9bb7521a2cf3a490643e1a4425e9e
Vortlan <utkumacit.55@gmail.com>
2022-04-22 11:16:47 -0500

    Lesson 10

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,5 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/singup').post(authController.createUser); // http://localhost:3000/users/singup
+router.route('/login').post(authController.loginUser);
 module.exports = router;
